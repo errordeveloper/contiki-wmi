4e40abfbdb0c4b98e483084c759ecb084c05d0c9
Ilya Dmitrichenko <errordeveloper@gmail.com>
2011-03-15 14:59:16 +0000

    modifie platform/freescale-1322xUSB

--- cpu/mc1322x/board/freescale-1322xusb.h
+++ cpu/mc1322x/board/freescale-1322xusb.h
@@ -33,8 +33,8 @@
  *
  */
 
-#ifndef BOARD_FREESCALE_NCB_H
-#define BOARD_FREESCALE_NCB_H
+#ifndef BOARD_FREESCALE_1322XUSB_H
+#define BOARD_FREESCALE_1322XUSB_H
 
 #define LED_RED    23
 #define LED_GREEN  24
@@ -44,10 +44,12 @@
 /* see http://devl.org/pipermail/mc1322x/2009-December/000162.html */
 /* for details about how to make this measurment */
 
+/* TODO: check these values!
+ * for now copy of econotag */
 /* Coarse tune: add 4pf */
 #define CTUNE_4PF 1
 /* Coarse tune: add 0-15 pf */
-#define CTUNE 8
+#define CTUNE 11
 /* Fine tune: add FTUNE * 156fF (FTUNE is 4bits) */
 #define FTUNE 15
 
