63fbe04a71e1af5ca71c4fc180af36665836750c
Ilya Dmitrichenko <errordeveloper@gmail.com>
2011-03-18 13:41:37 +0000

    woking on MIDI test code for UART2

---
+++ examples/mc1322x/midi/uart2-midi.h
@@ -0,0 +1,23 @@
+/* Firsly baud rate has to be set to 31250:
+ * use maxima script to solve this:
+ * % maxima -b mc1322x-baudrate-solver.mac
+ * See Section 13.4.2 of the manual for details
+ *
+ * For BAUD= 31250, PBUS= 2400000, UMOD= 9999
+ * and using 8X oversampling, we get:
+ * UINC = 103.15625
+ * and using 16X oversampling:
+ * UINC = 207.3125
+ * but with 1X it will be:
+ * UINC = 12.01953125
+ *
+ * Therefore without oversampling it should be
+ * more accurate.
+ */
+
+#include "uart1.h"
+#define MIDI_UART2_INC 12
+#define MIDI_UART2_MOD MOD
+#define MIDI_UART2_SAMP UCON_SAMP
+
+#define midi_uart_init() uart2_init(MIDI_UART2_INC,MIDI_UART2_MOD,MIDI_UART2_SAMP)
